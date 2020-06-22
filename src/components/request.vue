b42b53b94c63ac79b126d0c5a2280609497a3180
Nathan George Nacimento Gencissk <nathangngencissk@gmail.com>
2020-06-22 13:46:20 -0300

    Add timestamp to requests

--- src/components/request.vue
+++ src/components/request.vue
@@ -7,7 +7,7 @@
       <text :style="{fontWeight: 'bold', fontSize: 15, color: '#35654d'}">{{request.status}}</text>
     </nb-col>
     <nb-col>
-      <text :style="{fontSize: 13}">{{request.timestamp}}</text>
+      <text :style="{fontSize: 13}">{{requestDate}}</text>
     </nb-col>
   </nb-row>
 </template>
@@ -18,6 +18,13 @@ import Icon from "react-native-vector-icons/FontAwesome";
 export default {
   props: {
     request: Object
+  },
+  computed: {
+    requestDate() {
+      let unix_timestamp = this.request.timestamp;
+      let date = new Date(unix_timestamp * 1000);
+      return `${date.getDate()}/${date.getMonth()}`;
+    }
   },
   components: {
     Icon
